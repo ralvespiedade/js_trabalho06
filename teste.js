3d6048bc60f5eba3f234e843620697324789f886
Ricardo Alves <ralvespiedade@gmail.com>
2023-11-16 08:01:29 -0300

    initial commit

--- teste.js
+++ teste.js
@@ -1,22 +1,34 @@
-// Função tradicional sem parâmetros
-function saudacao() {
-  console.log("**** CALCULADORA QUE SÓ MULTIPLICA ****")
+const banco = {
+  conta: "123456",
+  saldo: 1000,
+  tipoDeConta: "corrente",
+  agencia: "7890",
+
+  buscarSaldo: function () {
+    return this.saldo
+  },
+
+  deposito: function (valor) {
+    this.saldo += valor
+    return `Depósito de R$${valor} realizado. Novo saldo: R$${this.saldo}`
+  },
+
+  saque: function (valor) {
+    if (valor > this.saldo) {
+      return "Saldo insuficiente para saque."
+    } else {
+      this.saldo -= valor
+      return `Saque de R$${valor} realizado. Novo saldo: R$${this.saldo}`
+    }
+  },
+
+  numeroDaConta: function () {
+    return this.conta
+  }
 }
 
-saudacao() // Chamando a função saudacao
-
-// Função tradicional com parâmetros e retorno
-function soma(a, b) {
-  return a + b
-}
-
-var resultado = soma(5, 3) // Chamando a função soma e armazenando o resultado
-console.log("O resultado da soma é:", resultado)
-
-// Arrow function com parâmetros e retorno
-const multiplicacao = (x, y) => {
-  return x * y
-}
-
-let resultadoMultiplicacao = multiplicacao(4, 6) // Chamando a arrow function multiplicacao e armazenando o resultado
-console.log("O resultado da multiplicação é:", resultadoMultiplicacao)
+// Testando os métodos do objeto banco
+console.log("Saldo atual:", banco.buscarSaldo())
+console.log(banco.deposito(500))
+console.log(banco.saque(300))
+console.log("Número da conta:", banco.numeroDaConta())
